11a81d989e6fcc44df3a4ae1f839b6e5b9f39a7c
Danish Alvi <dalvi@lilo6.science.ru.nl>
2020-02-08 01:54:22 +0100

    Generalized Triangles, WOOH!

--- proj.m
+++ proj.m
@@ -1,19 +1,24 @@
 G := Graph< 3 | {1, 2}, {2, 3}, {1, 3}>;
-X := Edges(G);
-w1w2 := X[1];
 
-for e in EdgeSet(G) do;
-	if not w1w2 eq e then
-		AddVertices(~G, 2);
-		V := Vertices(G);
-		u := V[#V];
-		v := V[#V - 1];
-		x := InitialVertex(e);
-		y := TerminalVertex(e);
-		AddEdge(~G, Index(u), Index(x));
-		AddEdge(~G, Index(u), Index(y));
-		AddEdge(~G, Index(v), Index(x));
-		AddEdge(~G, Index(v), Index(y));
-		RemoveEdge(~G, Index(InitialVertex(e)), Index(TerminalVertex(e)));
-	end if;
-end for;
+generalized_K3 := function(G)
+	X := Edges(G);
+        w1w2 := X[1];
+	for e in EdgeSet(G) do;
+		if not w1w2 eq e then
+			RemoveEdge(~G, Index(InitialVertex(e)), Index(TerminalVertex(e)));
+			AddVertices(~G, 2);
+			V := Vertices(G);
+			u := V[#V];
+			v := V[#V - 1];
+			x := InitialVertex(e);
+			y := TerminalVertex(e);
+			AddEdge(~G, Index(u), Index(x));
+			AddEdge(~G, Index(u), Index(y));
+			AddEdge(~G, Index(v), Index(x));
+			AddEdge(~G, Index(v), Index(y));
+		end if;
+	end for;
+	return G;
+end function;
+
+generalized_K3(G);
